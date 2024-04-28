d814fea8cef68e6d85d795ae1eaeae49e2f75b3b
arendellian13 <listener1381@outlook.com>
2024-04-28 11:58:20 +0800

    AC P10309

--- training/2024-4/P10309.cpp
+++ training/2024-4/P10309.cpp
@@ -10,9 +10,8 @@ const ll MAXN = 100010;
 const ll MOD = 998244353;
 ll n, e;
 pll edges[MAXN];
-map<pll, ll> w;
 vector<ll> tr[MAXN];
-ll dep[MAXN], fa[MAXN], top[MAXN], pref[MAXN], siz[MAXN];
+ll dis1[MAXN], dis2[MAXN];
 int main() {
   cin >> n;
   for (int i = 1; i <= n - 1; ++i) {
@@ -22,63 +21,40 @@ int main() {
   }
   cin >> e;
   e = e * n % MOD;
-  function<void(int)> dfs1 = [&](int u) -> void {
-    siz[u] = 1;
-    for (auto v : tr[u]) {
-      if (v == fa[u])
-        continue;
-      dep[v] = dep[u] + 1;
-      fa[v] = u;
-      dfs1(v);
-      siz[u] += siz[v];
-      pref[u] = siz[pref[u]] < siz[v] ? v : pref[u];
-    }
-  };
-  function<void(int, int)> dfs2 = [&](int u, int c) -> void {
-    top[u] = c;
-    if (pref[u])
-      dfs2(pref[u], c);
-    for (auto v : tr[u]) {
-      if (v == fa[u] || v == pref[u])
-        continue;
-      dfs2(v, v);
-    }
-  };
-  function<int(int, int)> lca = [&](int u, int v) -> int {
-    while (top[u] != top[v]) {
-      if (dep[top[u]] < dep[top[v]])
-        swap(u, v);
-      u = fa[top[u]];
-    }
-    return dep[u] > dep[v] ? v : u;
-  };
-  function<int(int, int)> dis = [&](int u, int v) -> int {
-    auto l = lca(u, v);
-    return dep[u] + dep[v] - 2 * dep[l];
-  };
   ll p = 0, q = 0;
   ll maxdep = 0;
-  function<void(int, int)> getd = [&](int u, int d) {
+  function<void(int, int, int)> dfs1 = [&](int u, int f, int d) {
     if (d > maxdep) {
+      p = u;
       maxdep = d;
-      q = u;
     }
     for (auto v : tr[u]) {
-      if (v == fa[u])
+      if (v == f)
         continue;
-      getd(v, d + 1);
+      dfs1(v, u, d + 1);
     }
   };
-  dep[1] = 1;
-  dfs1(1);
-  dfs2(1, 1);
-  for (auto i = 1; i <= n; ++i)
-    if (dep[p] < dep[i])
-      p = i;
-  getd(p, 1);
+  dfs1(1, 0, 1);
+  q = p;
+  maxdep = 0;
+  dfs1(q, 0, 1);
   auto sum = 0ll;
+  function<void(int, int, ll *)> getdis = [&](int u, int f, ll *dis) {
+    for (auto v : tr[u]) {
+      if (v == f)
+        continue;
+      dis[v] = dis[u] + 1;
+      getdis(v, u, dis);
+    }
+  };
+  getdis(p, 0, dis1);
+  getdis(q, 0, dis2);
+  // cout << p << ' ' << q << endl;
+  // for (auto i = 1; i <= n; ++i) {
+  // cout << i << ' ' << dis1[i] << ' ' << dis2[i] << endl;
+  // }
   for (auto i = 1; i <= n; ++i) {
-    sum = (sum + max(dis(i, p), dis(i, q))) % MOD;
+    sum = (sum + max(dis1[i], dis2[i])) % MOD;
   }
   auto qpow = [](ll a, ll b) {
     ll res = 1;
@@ -90,7 +66,13 @@ int main() {
     }
     return res;
   };
-  if (sum == 0) {
+  if (n == 2) {
+    auto inv = qpow(2, MOD - 2);
+    if (e != 0)
+      cout << e * inv % MOD << endl;
+    else
+      cout << MOD << endl;
+  } else if (sum == 0) {
     auto inv = qpow(n + 2, MOD - 2);
     auto h = e * inv % MOD;
     for (int i = 1; i <= n - 1; ++i) {
