9659ecbf7049d04e864b48786eea0fc42b7c994c
arendellian13 <listener1381@outlook.com>
2024-06-05 14:09:24 +0800

    update P8868

--- training/2024-5/P8868.cpp
+++ training/2024-5/P8868.cpp
@@ -24,14 +24,14 @@ struct Node {
   Info info;
   Tag tag;
 };
-Node *segt;
-ull *a, *b;
+Node segt[MAXN * 4];
+ull a[MAXN], b[MAXN];
 Info merge(const Info &lhs, const Info &rhs) {
   return {lhs.s + rhs.s, lhs.sx + rhs.sx, lhs.sy + rhs.sy, lhs.sxy + rhs.sxy,
           lhs.len + rhs.len};
 }
 Tag merge(const Tag &lhs, const Tag &rhs) {
-  Tag res{lhs};
+  Tag res = lhs;
   if (lhs.cx && lhs.cy) {
     res.mlen += rhs.mxy * lhs.cx * lhs.cy + rhs.mx * lhs.cx + rhs.my * lhs.cy +
                 rhs.mlen;
@@ -58,15 +58,15 @@ Info merge(const Info &val, const Tag &tag) {
   res.s += tag.mx * val.sx + tag.my * val.sy + tag.mxy * val.sxy +
            tag.mlen * val.len;
   if (tag.cx && tag.cy) {
-    res.sxy += val.len * tag.cx * tag.cy;
-    res.sx += val.len * tag.cx;
-    res.sy += val.len * tag.cy;
+    res.sxy = val.len * tag.cx * tag.cy;
+    res.sx = val.len * tag.cx;
+    res.sy = val.len * tag.cy;
   } else if (tag.cx) {
-    res.sxy = tag.cx * val.len;
-    res.sx += tag.cx * val.len;
+    res.sxy = tag.cx * val.sy;
+    res.sx = tag.cx * val.len;
   } else if (tag.cy) {
-    res.sxy = tag.cy * val.len;
-    res.sy += tag.cy * val.len;
+    res.sxy = tag.cy * val.sx;
+    res.sy = tag.cy * val.len;
   }
   return res;
 }
@@ -88,9 +88,6 @@ void build(ll cur, ll l, ll r) {
   segt[cur].mid = (l + r) / 2;
   if (l == r) {
     segt[cur].info.len = 1;
-    segt[cur].info.sxy = segt[cur].info.s = a[l] * b[l];
-    segt[cur].info.sx = a[l];
-    segt[cur].info.sy = b[l];
     return;
   }
   build(lson, l, sgmid);
@@ -98,8 +95,6 @@ void build(ll cur, ll l, ll r) {
   pushup(cur);
 }
 void update(ll cur, ll l, ll r, Tag t) {
-  cout << cur << ' ' << l << ' ' << r << ' ' << segt[cur].l << ' '
-       << segt[cur].r << endl;
   if (l <= segt[cur].l && segt[cur].r <= r) {
     segt[cur].info = merge(segt[cur].info, t);
     segt[cur].tag = merge(segt[cur].tag, t);
@@ -117,6 +112,7 @@ Info query(ll cur, ll l, ll r) {
     return segt[cur].info;
   }
   Info res;
+  pushdown(cur);
   if (l <= sgmid)
     res = merge(res, query(lson, l, r));
   if (sgmid + 1 <= r)
@@ -128,13 +124,12 @@ struct Query {
   ll l, r, pos, ans;
 };
 ll n, q;
-SegmentTree::Node segt[MAXN * 4];
-ull a[MAXN], b[MAXN];
 Query qs[MAXM];
 ull lefta[MAXN], leftb[MAXN];
-pair<ull, ll> stk1[MAXN], stk2[MAXN];
+int stk1[MAXN], stk2[MAXN];
 ll top1 = 0, top2 = 0;
 int main() {
+  using namespace SegmentTree;
   cin >> q >> n;
   for (int i = 1; i <= n; ++i) {
     cin >> a[i];
@@ -142,9 +137,6 @@ int main() {
   for (int i = 1; i <= n; ++i) {
     cin >> b[i];
   }
-  SegmentTree::segt = segt;
-  SegmentTree::a = a;
-  SegmentTree::b = b;
   SegmentTree::build(1, 1, n);
   cin >> q;
   for (int i = 1; i <= q; ++i) {
@@ -153,34 +145,26 @@ int main() {
   }
   sort(qs + 1, qs + q + 1,
        [](const Query &lhs, const Query &rhs) { return lhs.r < rhs.r; });
-  cout << "sorted" << endl;
-  a[0] = b[0] = (1ll << 63);
-  for (int j = n; j >= 0; --j) {
-    while (top1 && stk1[top1].first < a[j]) {
-      lefta[stk1[top1].second] = j + 1;
+  for (int j = 1; j <= n; ++j) {
+    while (top1 && a[stk1[top1]] < a[j])
       --top1;
-    }
-    stk1[++top1] = make_pair(a[j], j);
-    while (top2 && stk2[top2].first < b[j]) {
-      leftb[stk2[top2].second] = j + 1;
+    lefta[j] = stk1[top1] + 1;
+    while (top2 && b[stk2[top2]] < b[j])
       --top2;
-    }
-    stk2[++top2] = make_pair(b[j], j);
+    leftb[j] = stk2[top2] + 1;
+    stk1[++top1] = stk2[++top2] = j;
   }
   for (auto i = 1; i <= n; ++i) {
     cout << a[i] << ' ' << lefta[i] << ' ' << b[i] << ' ' << leftb[i] << endl;
   }
-  cout << "queued" << endl;
   for (int i = 1, j = 1; i <= n && j <= q; ++i) {
-    SegmentTree::update(1, lefta[i], i, SegmentTree::Tag{a[i], 0, 0, 0, 0, 0});
-    SegmentTree::update(1, leftb[i], i, SegmentTree::Tag{0, b[i], 0, 0, 0, 0});
-    SegmentTree::update(1, 1, i, SegmentTree::Tag{0, 0, 0, 0, 1, 0});
-    cout << "updated" << endl;
+    update(1, lefta[i], i, Tag{a[i], 0, 0, 0, 0, 0});
+    update(1, leftb[i], i, Tag{0, b[i], 0, 0, 0, 0});
+    update(1, 1, i, Tag{0, 0, 0, 0, 1, 0});
     while (qs[j].r == i && j <= q) {
-      qs[j].ans = SegmentTree::query(1, qs[j].l, i).sxy;
+      qs[j].ans = query(1, qs[j].l, i).s;
       ++j;
     }
-    cout << "queried" << endl;
   }
   sort(qs + 1, qs + q + 1,
        [](const Query &lhs, const Query &rhs) { return lhs.pos < rhs.pos; });
